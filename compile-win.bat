e39dc5645a4fec39d2613d33dab4dd62c3a22dd3
Tumble <tumblegamer@gmail.com>
2018-06-04 22:45:48 +0100

    extract zip and compile

--- compile-win.bat
+++ compile-win.bat
@@ -1,2 +1,2 @@
-PowerShell.exe -ExecutionPolicy Bypass -Command "Compress-Archive -Path * -DestinationPath resources.zip"
+PowerShell.exe -ExecutionPolicy Bypass -Command "Compress-Archive -Path * -DestinationPath resources.zip" -Update
 PAUSE
