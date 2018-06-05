267bd492f14bc1ca844d2965118aedcaa09f70aa
Tumble <tumblegamer@gmail.com>
2018-06-05 11:21:49 +0100

    Revert "extract zip and compile"
    This reverts commit e39dc5645a4fec39d2613d33dab4dd62c3a22dd3.

--- compile-win.bat
+++ compile-win.bat
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 del resources.zip
 PowerShell.exe -ExecutionPolicy Bypass -Command "Compress-Archive -Path resources/* -DestinationPath resources.zip -Update"
+=======
+PowerShell.exe -ExecutionPolicy Bypass -Command "Compress-Archive -Path * -DestinationPath resources.zip"
+>>>>>>> parent of e39dc56... extract zip and compile
 PAUSE
